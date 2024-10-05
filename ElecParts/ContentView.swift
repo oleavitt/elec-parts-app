8a57726ce45a4bdc2f5d3729ea66bec918fcb88a
Oren Leavitt <oleavitt@ix.netcom.com>
2024-10-04 23:03:19 -0500

    Add starter app

---
+++ ElecParts/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ElecParts
+//
+//  Created by Oren Leavitt on 10/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ElecParts/ElecPartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElecPartsApp.swift
+//  ElecParts
+//
+//  Created by Oren Leavitt on 10/4/24.
+//
+
+import SwiftUI
+
+@main
+struct ElecPartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
